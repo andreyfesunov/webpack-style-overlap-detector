45945c7b663cddfab002737ae0b4ff9b763b13d1
andreyfesunov <fsnv04@gmail.com>
2025-03-24 13:43:49 +0300

    fix(1.0.1): remove dist from repo, add css as dependency

--- dist/analyzer.d.ts
+++
@@ -1,8 +0,0 @@
-export interface StyleOverlapAnalyzerOptions {
-    readonly whitelist?: readonly string[];
-}
-export declare class StyleOverlapAnalyzer {
-    private readonly _options;
-    constructor(options: StyleOverlapAnalyzerOptions);
-    analyze(css: string): readonly string[];
-}
